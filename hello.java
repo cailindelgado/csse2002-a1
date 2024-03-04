f438b865f329c8a10530acf1768ebd8cc8e9d62a
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-03-05 08:40:53 +1000

    testing

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
+        if (true) {
+            System.out.println("fart");
+        }
         System.out.println("Hello");
     }
 }
