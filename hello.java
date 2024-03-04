4d50d4eda0c90fbe01ca6cf90da31eefd200f3a5
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-03-05 08:45:30 +1000

    wow

--- hello.java
+++
@@ -1,8 +0,0 @@
-public class hello {
-    public static void main(String[] args) {
-        if (true) {
-            System.out.println("fart");
-        }
-        System.out.println("Hello");
-    }
-}
