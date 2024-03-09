5e6ede546126b73894e2550c684c35544bf54cf9
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-03-09 13:28:55 +1000

    worked on stage 0

--- provided/src/sheep/sheets/FixedSheet.java
+++ provided/src/sheep/sheets/FixedSheet.java
@@ -4,18 +4,31 @@ import sheep.core.SheetView;
 import sheep.core.UpdateResponse;
 import sheep.core.ViewElement;
 
-//do we need to create a new file for sheep.sheets? 
-//are we allowed to add imports?
-//
-
 public class FixedSheet {
 
-    public static ViewElement formulaAt(int x, int y) {
-        System.out.println("Returns Viewelement");
+    /*
+     * @param row The row index to update
+     * @param column The column index to update
+     * @param input The value as a string to replace
+     *
+     * @returns An approximately formatted cell based on whether it is highlighted or not.
+     *
+     * @requires 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
+     */
+    public static ViewElement formulaAt(int row, int column) {
+
     }
 
-    public static ViewElement valueAt(int x, int y) {
-        System.out.println("Returns Viewelement");
+    /*
+     * @param row The row index to update
+     * @param column The column index to update
+     * @param input The value as a string to replace
+     *
+     * @returns An approximately formatted cell based on whether it is highlighted or not.
+     *
+     * @requires 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
+     */
+    public static ViewElement valueAt(int row, int column) {
 
     }
 
@@ -34,14 +47,40 @@ public class FixedSheet {
     }
 
     /*
-     * @params row The row index to update
+     * Attempts to update a cell in the position given
+     *
+     * @param row The row index to update
      * @param column The column index to update
      * @param input The value as a string to replace
      * 
-     * @returns 
+     * @returns "A failed update as the sheet is view only"
+     *
+     * @requires 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
      */
-    public static UpdateResponse update(int x, int y, String conents) {
-        System.out.println("A failed update as the sheet is view only");
+    public static UpdateResponse update(int row, int column, String input) {
+        if (checker(row, column, input)) {
+            return UpdateResponse.fail("A failed update as the sheet is view only");
+        }
+        return UpdateResponse.success();
+    }
+
+
+    /*
+     * Performs a check on the given inputs and returns the appropriate response
+     * performs the following check: 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
+     *
+     * @param row The row index to update
+     * @param column The column index to update
+     * @param input The value as a string to replace
+     *
+     * @returns true or false, based off of the
+     *
+     *
+     */
+    private static boolean checker(int row, int column, String input) {
+        boolean req1 = ((0 < row) || (0 == row)) && (row < getRows());
+        boolean req2 = ((0 < column) || (0 == column)) && (column < getColumns());
+        return (input != null && req1 && req2);
     }
 
 }
