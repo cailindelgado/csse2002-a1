24ce1e1d08e1c0ad1ab3f1d3a497a361f0b1ee0b
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-03-10 19:56:41 +1000

    function documentation done

--- provided/src/sheep/sheets/FixedSheet.java
+++ provided/src/sheep/sheets/FixedSheet.java
@@ -1,60 +1,67 @@
 package sheep.sheets;
+
 import sheep.core.SheetUpdate;
 import sheep.core.SheetView;
 import sheep.core.UpdateResponse;
 import sheep.core.ViewElement;
 
+/**
+ * Spreadsheet that has fixed values in every cell.
+ *
+ * @extends Object
+ * @implements {@link SheetView} and {@link SheetUpdate}
+ */
 public class FixedSheet {
 
-    /*
+    /**
+     * Determines the formula to display at given cell.
+     *
      * @param row The row index to update
      * @param column The column index to update
-     * @param input The value as a string to replace
-     *
-     * @returns An approximately formatted cell based on whether it is highlighted or not.
-     *
+     * @return An approximately formatted cell based on whether it is highlighted or not.
      * @requires 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
      */
     public static ViewElement formulaAt(int row, int column) {
 
     }
 
-    /*
+    /**
+     * Determines the value to display at given cell.
+     *
      * @param row The row index to update
      * @param column The column index to update
-     * @param input The value as a string to replace
-     *
-     * @returns An approximately formatted cell based on whether it is highlighted or not.
-     *
+     * @return An approximately formatted cell based on whether it is highlighted or not.
      * @requires 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
      */
     public static ViewElement valueAt(int row, int column) {
 
     }
 
-    /*
-     * @returns 6
+    /**
+     * The number of rows in the sheet.
+     *
+     * @return 6
      */
     public static int getRows() {
         return 6;
     }
 
-    /*
-     * @returns 6
+    /**
+     * The number of columns in the sheet.
+     *
+     * @return 6
      */
     public static int getColumns() {
         return 6;
     }
 
-    /*
+    /**
      * Attempts to update a cell in the position given
      *
      * @param row The row index to update
      * @param column The column index to update
      * @param input The value as a string to replace
-     * 
-     * @returns "A failed update as the sheet is view only"
-     *
+     * @return "A failed update as the sheet is view only"
      * @requires 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
      */
     public static UpdateResponse update(int row, int column, String input) {
@@ -65,17 +72,14 @@ public class FixedSheet {
     }
 
 
-    /*
+    /**
      * Performs a check on the given inputs and returns the appropriate response
      * performs the following check: 0 <= row < getRows(), 0 <= column < getColumns(), input!= null
      *
-     * @param row The row index to update
-     * @param column The column index to update
-     * @param input The value as a string to replace
-     *
-     * @returns true or false, based off of the
-     *
-     *
+     * @param row The row index
+     * @param column The column index
+     * @param input  A String input
+     * @return true or false, based off of the
      */
     private static boolean checker(int row, int column, String input) {
         boolean req1 = ((0 < row) || (0 == row)) && (row < getRows());
