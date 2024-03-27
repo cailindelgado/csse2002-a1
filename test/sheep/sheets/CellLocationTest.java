a3c30ae0275c6320781f88970f25852c0355a52e
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-03-27 17:02:53 +1000

    CellLocation testing done

--- test/sheep/sheets/CellLocationTest.java
+++ test/sheep/sheets/CellLocationTest.java
@@ -1,4 +1,20 @@
 package sheep.sheets;
 
+import sheep.sheets.CellLocation;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
 public class CellLocationTest {
+
+    @Test
+    public void holdsValue() {
+        CellLocation testing = new CellLocation(4, 'A');
+        assertEquals(Optional.of(new CellLocation(0,'A')), CellLocation.maybeReference("A0"));
+
+    }
+
 }
